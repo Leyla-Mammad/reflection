d97167eee063b50a76a1750979b8fb66907189a1
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-11-17 02:02:03 +0400

    reflection

---
+++ reflection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Student
+{
+    private static int lastStudentAge = 0;
+    public int Age { get; private set; }
+
+    public Student()
+    {
+        Age = ++lastStudentAge;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student tural = new Student();
+        Console.WriteLine(tural.Age);
+
+        Student suleiman = new Student();
+        Console.WriteLine(suleiman.Age);
+
+        Student gafar = new Student();
+        Console.WriteLine(gafar.Age); 
+
+        Student semed = new Student();
+        Console.WriteLine(semed.Age); 
+    }
+}
